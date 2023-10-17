a15d199bbca45c869613447210b20c48e9fd9662
teste-agua-edu <gui.2001@hotmail.com>
2023-10-16 22:22:33 -0300

    chore: add nextAuth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>
-        Testeee
-      </h1>
-    </div>
+    <div>home</div>
   )
 }
