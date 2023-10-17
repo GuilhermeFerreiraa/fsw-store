193feceaa8f154286089386471c0cd5b36d83183
teste-agua-edu <gui.2001@hotmail.com>
2023-10-17 20:32:23 -0300

    feat: add first discount banner to homepage

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useSession } from "next-auth/react"
+import Image from "next/image";
+
+export default function Home() {
+  const { data } = useSession();
+
+  return (
+    <div className="p-5">
+      <Image
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-auto w-full"
+        src="/banner_home-01.png"
+        alt="Até 55% de desconto esse mês!" />
+    </div>
+  )
+}
