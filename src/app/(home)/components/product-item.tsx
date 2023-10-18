9daefa61c59146108985e678ff64880c8ef002a2
teste-agua-edu <gui.2001@hotmail.com>
2023-10-18 20:16:00 -0300

    feat: add ensure user goes to product details page on click

--- src/app/(home)/components/product-item.tsx
+++ src/app/(home)/components/product-item.tsx
@@ -3,6 +3,7 @@ import { ProductsWithTotalPrice } from "@/helpers/product";
 import { Product } from "@prisma/client";
 import { ArrowDown01Icon, ArrowDownIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface ProductItemProps {
  product: ProductsWithTotalPrice;
@@ -10,51 +11,53 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
  return (
-  <div className="flex flex-col gap-4">
+  <Link href={`/product/${product.slug}`}>
+   <div className="flex flex-col gap-4">
 
-   <div className="relative bg-accent rounded-lg h-[170px] w-full flex items-center justify-center">
-    <Image
-     width={0}
-     height={0}
-     sizes="100vw"
-     alt={product.name}
-     src={product.imageUrls[0]}
-     className="h-auto max-h-[70%] w-auto max-w-[80%]"
-     style={{ objectFit: 'contain' }}
-    />
+    <div className="relative bg-accent rounded-lg h-[170px] w-full flex items-center justify-center">
+     <Image
+      width={0}
+      height={0}
+      sizes="100vw"
+      alt={product.name}
+      src={product.imageUrls[0]}
+      className="h-auto max-h-[70%] w-auto max-w-[80%]"
+      style={{ objectFit: 'contain' }}
+     />
 
-    {product.discountPercentage && (
-     <Badge className="absolute left-3 top-3 px-2 py-[2px]">
-      <ArrowDownIcon size={14} />
-      {product.discountPercentage}%
-     </Badge>
-    )}
-   </div>
-
-   <div className="flex flex-col gap-1">
-    <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">
-     {product.name}
-    </p>
-
-    <div className="flex items-center gap-2 overflow-hidden whitespace-nowrap text-ellipsis">
-     {product.discountPercentage > 0 ? (
-      <>
-       <p className="font-semibold">
-        R$ {Number(product.totalPrice).toFixed(2).replace('.', ',')}
-       </p>
-
-       <p className="opacity-75 line-through text-xs overflow-hidden whitespace-nowrap text-ellipsis">
-        R$ {Number(product.basePrice).toFixed(2).replace('.', ',')}
-       </p>
-      </>
-     ) : (
-      <p className="text-sm font-semibold">
-       R$ {Number(product.basePrice).toFixed(2).replace('.', ',')}
-      </p>
+     {product.discountPercentage && (
+      <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+       <ArrowDownIcon size={14} />
+       {product.discountPercentage}%
+      </Badge>
      )}
     </div>
+
+    <div className="flex flex-col gap-1">
+     <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">
+      {product.name}
+     </p>
+
+     <div className="flex items-center gap-2 overflow-hidden whitespace-nowrap text-ellipsis">
+      {product.discountPercentage > 0 ? (
+       <>
+        <p className="font-semibold">
+         R$ {Number(product.totalPrice).toFixed(2).replace('.', ',')}
+        </p>
+
+        <p className="opacity-75 line-through text-xs overflow-hidden whitespace-nowrap text-ellipsis">
+         R$ {Number(product.basePrice).toFixed(2).replace('.', ',')}
+        </p>
+       </>
+      ) : (
+       <p className="text-sm font-semibold">
+        R$ {Number(product.basePrice).toFixed(2).replace('.', ',')}
+       </p>
+      )}
+     </div>
+    </div>
    </div>
-  </div>
+  </Link>
  );
 }
 
