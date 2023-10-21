b404edda75e9cd39c6e1c812a0c1614829d6ab12
teste-agua-edu <gui.2001@hotmail.com>
2023-10-21 20:28:16 -0300

    fix: fix route payment-success

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -1,13 +1,12 @@
-import { prismaClient } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
- apiVersion: '2023-10-16',
-})
+ apiVersion: "2023-10-16",
+});
 
 export const POST = async (request: Request) => {
- const signature = request.headers.get('stripe-signature')
+ const signature = request.headers.get("stripe-signature");
 
  if (!signature) {
   return NextResponse.error();
@@ -26,16 +25,13 @@ export const POST = async (request: Request) => {
  }
 
  if (event.type === "checkout.session.completed") {
-  const sessionsWithLineItens = await stripe.checkout.sessions.retrieve(
+  const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
    event.data.object.id,
    {
-    expand: ['line_items']
+    expand: ["line_items"],
    },
   );
-
-  // await prismaClient
-
-  console.log(sessionsWithLineItens.line_items);
+  const lineItems = sessionWithLineItems.line_items;
  }
 
  return NextResponse.json({ received: true });
