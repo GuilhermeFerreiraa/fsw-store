f3241d8251d67e92a60f14ba23207e188474b6e5
teste-agua-edu <gui.2001@hotmail.com>
2023-10-21 21:40:58 -0300

    fix: url success route

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -12,8 +12,8 @@ export const createCheckout = async (products: CartProduct[]) => {
   const checkout = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
-    success_url: process.env.HOST_URL,
-    cancel_url: process.env.HOST_URL,
+    success_url: `${process.env.HOST_URL}`,
+    cancel_url: `${process.env.HOST_URL}`,
     line_items: products.map((product) => {
       return {
         price_data: {
